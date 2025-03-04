7d57f5ac1f37268672999d43e8aec57759650542
neelshah532 <neelshah0503@gmail.com>
2025-03-04 12:37:44 +0530

    ref: refectore the folder structure add the style folder for css

---
+++ src/components/AdressDetailsFormInput.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdressDetailsFormInput = () => {
+  return (
+    <div>AdressDetailsFormInput</div>
+  )
+}
+
+export default AdressDetailsFormInput
---
+++ src/components/BasicDetailsFormInput.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BasicDetailsFormInput = () => {
+  return (
+    <div>BasicDetailsFormInput</div>
+  )
+}
+
+export default BasicDetailsFormInput
